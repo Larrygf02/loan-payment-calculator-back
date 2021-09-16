a6fac68bc7667e6468cdccc788d8512a86b160b4
larrygf02 <raulgf_02@hotmail.com>
2021-09-15 21:52:34 -0500

    Get days

--- app/services/process/calculate_payment.py
+++ app/services/process/calculate_payment.py
@@ -1,10 +1,10 @@
-
+from datetime import datetime, timedelta
 def run(data):
     is_double_fee = data.get('is_double_fee')
     if not is_double_fee:
         result = calculate_with_simple_fee(data)
     else:
-        result = []
+        result = calculate_with_double_fee(data)
     return result
 
 def calculate_with_simple_fee(data):
@@ -29,4 +29,24 @@ def calculate_with_simple_fee(data):
         }
         result.append(result_item)
     return result
+        
+def calculate_with_double_fee(data):
+    dues = data.get('loan_term')
+    days = get_days_paid(dues)
+    print(days)
+
+def get_days_paid(dues):
+    days = []
+    now = datetime.now()
+    day = 28 if now.day > 28 else now.day
+    month = now.month
+    year = now.year
+    for _ in range(1, dues):
+        if month == 12:
+            month = 1
+            year += 1
+        else:
+            month += 1
+        days.append(datetime(year, month, day))
+    return days
         
